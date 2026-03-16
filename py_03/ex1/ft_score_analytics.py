b0c82d2a2fe4af6575264bd62553b620aa4665df
amine-XB8999999999999 <aminebossaboune@gmail.com>
2026-03-16 03:16:36 +0000

    ex02

--- py_03/ex1/ft_score_analytics.py
+++ py_03/ex1/ft_score_analytics.py
@@ -1,18 +1,18 @@
 import sys
 
-"""this is a programe where we discover more about list
+
+def main() -> None:
+    """
+    this is a fct to discover more about list
     we store it and organize it as a games scores:
     Calculate some basic stats on this list and  Handle the udge case
-"""
-
-if __name__ == '__main__':
-
+    """
     player_list = []
     try:
         print("=== Player Score Analytics ===")
-        if (len(sys.argv) > 1):
+        if len(sys.argv) > 1:
             i = 1
-            while (i < len(sys.argv)):
+            while i < len(sys.argv):
                 player_list = player_list + [int(sys.argv[i])]
                 i += 1
             print(f"Scores processed: {player_list}")
@@ -23,7 +23,11 @@ if __name__ == '__main__':
             print(f"Low score: {min(player_list)}")
             print(f"Score range: {max(player_list) - min(player_list)}")
         else:
-            print("No scores provided. Usage: ", end='')
+            print("No scores provided. Usage: ", end="")
             print("python3 ft_score_analytics.py <score1> <score2> ...")
     except ValueError:
         print("oops, you have an value error.")
+
+
+if __name__ == "__main__":
+    main()
