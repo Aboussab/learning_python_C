5375cbe8b4ef88f35697891931957bd438c3d64a
amine boussaboune <62100367+amine-XB8999999999999@users.noreply.github.com>
2026-04-13 18:29:46 +0100

    finishing the 3rd part of the py06

--- py_06/alchemy/__init__.py
+++ py_06/alchemy/__init__.py
@@ -1,4 +1,7 @@
 from .elements import create_air  # noqa: F401
+from .potions import healing_potion as heal  # noqa: F401
+from .potions import strength_potion  # noqa: F401
+
 
 # This stands for "No Quality Assurance" for error code F401 (unused import).
-__all__ = ["create_air"]
+__all__ = ["create_air", "potion"]
---
+++ py_06/ft_transmutation_1.py
@@ -0,0 +1,4 @@
+import  alchemy.transmutation as pt
+
+print("=== Transmutation 1 ===")
+print("Import transmutation module directly")
