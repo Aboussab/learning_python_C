492e7872b960c9b565edea96ec199ab5c56a98b1
amine-XB8999999999999 <aminebossaboune@gmail.com>
2026-04-16 11:58:36 +0100

    kind of funny

--- py_07/ex0/createur.py
+++ py_07/ex0/createur.py
@@ -17,22 +17,22 @@ class Creature(ABC):
 
 class Flameling(Creature):
     def attack(self) -> str:
-        pass
+        return f"{self.name} uses Ember!"
 
 
 class Pyrodon(Creature):
     def attack(self) -> str:
-        pass
+        return f"{self.name} uses  Flamethrower!"
 
 
 class Aquabub(Creature):
     def attack(self) -> str:
-        pass
+        return f"{self.name} uses Water Gun!"
 
 
 class Torragon(Creature):
     def attack(self) -> str:
-        pass
+        return f"{self.name} uses Pump!"
 
 
 class CreatureFactory(ABC):
